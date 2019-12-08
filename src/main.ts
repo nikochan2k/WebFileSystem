0ab667108f86eb25e74f4d10d6cb203f18660101
Yoshihiro Seki <nikochan2k@gmail.com>
2019-12-08 10:23:57 +0900

    create WebFileSystem class

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,74 @@
 import {
-  LocalFileSystem,
-  FileSystemCallback,
-  ErrorCallback,
   EntryCallback,
-  Entry
+  ErrorCallback,
+  FileSystemCallback,
+  LocalFileSystem
 } from "./filesystem";
-import { INVALID_MODIFICATION_ERR, NOT_FOUND_ERR } from "./FileError";
+import { IdbGlobal } from "./idb/IdbGlobal";
 
-const nativeFileSystem =
-  window.requestFileSystem || window.webkitRequestFileSystem;
+export class WebFileSystem implements LocalFileSystem {
+  private localFileSystem = {} as LocalFileSystem;
 
-const nativeFileSystemSync =
-  window.requestFileSystemSync || window.webkitRequestFileSystemSync;
+  constructor(provider?: string, options?: any) {
+    if (!provider) {
+      this.constructNativeFileSystem();
+      if (this.localFileSystem.requestFileSystem) {
+        return;
+      }
+      provider = "idb";
+    }
+
+    switch (provider) {
+      case "native":
+        this.constructNativeFileSystem();
+        return;
+      case "idb":
+        this.localFileSystem = new IdbGlobal();
+        return;
+    }
+
+    throw new Error(`Illegal provider: ${provider}`);
+  }
+
+  private constructNativeFileSystem() {
+    this.localFileSystem.requestFileSystem =
+      window.requestFileSystem || window.webkitRequestFileSystem;
+    this.localFileSystem.resolveLocalFileSystemURL =
+      window.resolveLocalFileSystemURL;
+    this.localFileSystem.TEMPORARY = window.TEMPORARY;
+    this.localFileSystem.PERSISTENT = window.PERSISTENT;
+  }
+
+  get TEMPORARY() {
+    return this.localFileSystem.TEMPORARY;
+  }
+  get PERSISTENT() {
+    return this.localFileSystem.PERSISTENT;
+  }
+
+  requestFileSystem(
+    type: number,
+    size: number,
+    successCallback: FileSystemCallback,
+    errorCallback?: ErrorCallback
+  ): void {
+    return this.localFileSystem.requestFileSystem(
+      type,
+      size,
+      successCallback,
+      errorCallback
+    );
+  }
+
+  resolveLocalFileSystemURL(
+    url: string,
+    successCallback: EntryCallback,
+    errorCallback?: ErrorCallback
+  ): void {
+    return this.localFileSystem.resolveLocalFileSystemURL(
+      url,
+      successCallback,
+      errorCallback
+    );
+  }
+}
