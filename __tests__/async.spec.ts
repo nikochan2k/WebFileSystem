368b0d34566f63efacff0361d9cf3308150332c5
Yoshihiro Seki <nikochan2k@gmail.com>
2019-12-11 23:35:46 +0900

    テストを追加

--- __tests__/async.spec.ts
+++ __tests__/async.spec.ts
@@ -1,6 +1,8 @@
 require("fake-indexeddb/auto");
+
 import { WebFileSystemAsyncFactory } from "../src/WebFileSystemAsyncFactory";
 import { FileSystemAsync } from "../src/filesystem";
+import { NOT_FOUND_ERR } from "../src/FileError";
 
 let fs: FileSystemAsync;
 beforeAll(async () => {
@@ -49,6 +51,47 @@ test("add text file", async done => {
     reader.readAsText(file);
   });
   expect(str).toBe("hogefuga");
+
+  done();
+});
+
+test("create dir", async done => {
+  const dirEntry = await fs.root.getDirectory("folder", {
+    create: true,
+    exclusive: true
+  });
+  expect(dirEntry.isFile).toBe(false);
+  expect(dirEntry.isDirectory).toBe(true);
+  expect(dirEntry.name).toBe("folder");
+
+  done();
+});
+
+test("create file in the dir", async done => {
+  const dirEntry = await fs.root.getDirectory("folder");
+  const fileEntry = await dirEntry.getFile("in.txt", {
+    create: true,
+    exclusive: true
+  });
+  expect(fileEntry.fullPath).toBe("/folder/in.txt");
+  try {
+    await dirEntry.getFile("out.txt");
+    fail();
+  } catch (e) {
+    expect(e).toBe(NOT_FOUND_ERR);
+  }
+
+  done();
+});
+
+test("readdir", async done => {
+  const reader = fs.root.createReader();
+  const entries = await reader.readEntries();
+  let names = ["empty.txt", "test.txt", "folder"];
+  for (const entry of entries) {
+    names = names.filter(name => name !== entry.name);
+  }
+  expect(names.length).toBe(0);
 
   done();
 });
